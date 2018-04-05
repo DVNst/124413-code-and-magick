a583161dc3407bc923ec50aa5d442fe092a3ab1d
DVNst <dvn-studio@mail.ru>
2018-04-05 17:57:31 +0300

    выносит получение случайного цвета в отдельную функцию + мелкие правки

--- js/stat.js
+++ js/stat.js
@@ -5,7 +5,7 @@ var CLOUD_HEIGHT = 270;
 var CLOUD_X = 100;
 var CLOUD_Y = 10;
 var GAP = 10;
-var FONT_GAP = 20;
+var TEXT_GAP = 20;
 var TEXT_HEIGHT = 20;
 
 var BAR_WIDTH = 40;
@@ -16,20 +16,21 @@ var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
 
   var index = CLOUD_WIDTH / 14;
+  var radian = (Math.PI / 180);
 
   ctx.beginPath();
-  ctx.arc(x + CLOUD_WIDTH - index / 2, y + index / 2, index / 2, 0, (Math.PI / 180) * 360);
+  ctx.arc(x + CLOUD_WIDTH - index / 2, y + index / 2, index / 2, 0, radian * 360);
   ctx.moveTo(x + index * 2, y);
   ctx.lineTo(x + index * 10, y);
-  ctx.arc(x + index * 10, y + index, index, (Math.PI / 180) * 270, (Math.PI / 180) * 360);
-  ctx.arc(x + index * 10 + 50, y + index + 40, 42, (Math.PI / 180) * 240, (Math.PI / 180) * 30);
-  ctx.arc(x + index * 10 + 90, y + index + 90, index, (Math.PI / 180) * 270, (Math.PI / 180) * 360);
+  ctx.arc(x + index * 10, y + index, index, radian * 270, radian * 360);
+  ctx.arc(x + index * 10 + 50, y + index + 40, 42, radian * 240, radian * 30);
+  ctx.arc(x + index * 10 + 90, y + index + 90, index, radian * 270, radian * 360);
   ctx.lineTo(x + CLOUD_WIDTH, y + CLOUD_HEIGHT - index);
-  ctx.arc(x + CLOUD_WIDTH - index, y + CLOUD_HEIGHT - index, index, 0, (Math.PI / 180) * 90);
+  ctx.arc(x + CLOUD_WIDTH - index, y + CLOUD_HEIGHT - index, index, 0, radian * 90);
   ctx.lineTo(x + index, y + CLOUD_HEIGHT);
-  ctx.arc(x + index, y + CLOUD_HEIGHT - index, index, (Math.PI / 180) * 90, (Math.PI / 180) * 180);
+  ctx.arc(x + index, y + CLOUD_HEIGHT - index, index, radian * 90, radian * 180);
   ctx.lineTo(x, y + index * 2);
-  ctx.arc(x + index * 2, y + index * 2, index * 2, (Math.PI / 180) * 180, (Math.PI / 180) * 270);
+  ctx.arc(x + index * 2, y + index * 2, index * 2, radian * 180, radian * 270);
   ctx.closePath();
   ctx.fill();
 };
@@ -50,31 +51,34 @@ var getMaxElement = function (arr) {
   return maxElement;
 };
 
+var getRandomColor = function (names) {
+  if (names === 'Вы') {
+    return 'rgba(255, 0, 0, 1)';
+  }
+  return 'hsl(240, ' + Math.random() * 100 + '%, 50%)';
+}
+
 window.renderStatistics = (function (ctx, names, times) {
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
-  renderCloud(ctx, CLOUD_X, CLOUD_Y, 'rgba(256, 256, 256, 1.0)');
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, 'rgba(255, 255, 255, 1)');
   ctx.stroke();
 
   ctx.fillStyle = '#000';
   ctx.font = '16px PT Mono';
 
-  ctx.fillText('Ура вы победили!', CLOUD_X + FONT_GAP * 3, CLOUD_Y + FONT_GAP);
-  ctx.fillText('Список результатов:', CLOUD_X + FONT_GAP * 2, CLOUD_Y + FONT_GAP + TEXT_HEIGHT);
+  ctx.fillText('Ура вы победили!', CLOUD_X + TEXT_GAP * 3, CLOUD_Y + TEXT_GAP);
+  ctx.fillText('Список результатов:', CLOUD_X + TEXT_GAP * 2, CLOUD_Y + TEXT_GAP + TEXT_HEIGHT);
 
   var maxTime = getMaxElement(times);
 
   for (var i = 0; i < names.length; i++) {
     ctx.fillStyle = '#000';
-    ctx.fillText(names[i], CLOUD_X + FONT_GAP + (BAR_WIDTH + BAR_SPACE) * i, CLOUD_HEIGHT - GAP);
+    ctx.fillText(names[i], CLOUD_X + TEXT_GAP + (BAR_WIDTH + BAR_SPACE) * i, CLOUD_HEIGHT - GAP);
 
-    ctx.fillText(Math.round(times[i]), CLOUD_X + FONT_GAP + (BAR_WIDTH + BAR_SPACE) * i, CLOUD_HEIGHT - TEXT_HEIGHT - GAP * 2 - BAR_HEIGHT * times[i] / maxTime);
+    ctx.fillText(Math.round(times[i]), CLOUD_X + TEXT_GAP + (BAR_WIDTH + BAR_SPACE) * i, CLOUD_HEIGHT - TEXT_HEIGHT - GAP * 2 - BAR_HEIGHT * times[i] / maxTime);
 
-    ctx.fillStyle = 'hsl(240, ' + Math.random() * 100 + '%, 50%)';
-    if (names[i] === 'Вы') {
-      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-    }
-    // ctx.fillStyle = (names[i]==='Вы') ? "rgba(255, 0, 0, 1)" : "hsl(240, " + Math.random() * 100 + "%, 50%)";
+    ctx.fillStyle = getRandomColor(names[i]);
 
-    ctx.fillRect(CLOUD_X + FONT_GAP + (BAR_WIDTH + BAR_SPACE) * i, CLOUD_HEIGHT - TEXT_HEIGHT - GAP - BAR_HEIGHT * times[i] / maxTime, BAR_WIDTH, BAR_HEIGHT * times[i] / maxTime);
+    ctx.fillRect(CLOUD_X + TEXT_GAP + (BAR_WIDTH + BAR_SPACE) * i, CLOUD_HEIGHT - TEXT_HEIGHT - GAP - BAR_HEIGHT * times[i] / maxTime, BAR_WIDTH, BAR_HEIGHT * times[i] / maxTime);
   }
 });
